475713e3a1f60b025003ba307be53963e3c00776
Fedor Sumkin <qosys.net@gmail.com>
2013-07-27 22:39:24 +0400

    Create get_file_contents.cpp

---
+++ cpp/files/get_file_contents.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <string>
+#include <cerrno>
+
+// http://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+
+std::string get_file_contents(const char *filename)
+{
+  std::ifstream in(filename, std::ios::in | std::ios::binary);
+  if (in)
+  {
+    std::string contents;
+    in.seekg(0, std::ios::end);
+    contents.resize(in.tellg());
+    in.seekg(0, std::ios::beg);
+    in.read(&contents[0], contents.size());
+    in.close();
+    return(contents);
+  }
+  throw(errno);
+}
