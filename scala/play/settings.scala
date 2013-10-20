00272e16e984c15957162593e86d3966841d4d1c
Fedor Sumkin <qosys.net@gmail.com>
2013-10-21 00:18:15 +0400

    Create settings.scala

---
+++ scala/play/settings.scala
@@ -0,0 +1,6 @@
+val configThing = Play.application.configuration.getString("configThing.key").getOrElse(throw new RuntimeException("Setting configTHing.key not defined"))
+
+...
+
+def stringSetting(key: String): String = Play.application.configuration.getString(key).getOrElse(throw new RuntimeException(s"Setting $key not defined"))
+val configThing = stringSetting("configThing.key")
