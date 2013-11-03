1e09ceeca7c525820ffe007f71e10a0848c6eb1e
Fedor Sumkin <qosys.net@gmail.com>
2013-11-03 16:52:23 +0300

    Update settings.scala
    current/application difference?

--- scala/play/settings.scala
+++ scala/play/settings.scala
@@ -4,3 +4,6 @@ val configThing = Play.application.configuration.getString("configThing.key").ge
 
 def stringSetting(key: String): String = Play.application.configuration.getString(key).getOrElse(throw new RuntimeException(s"Setting $key not defined"))
 val configThing = stringSetting("configThing.key")
+
+
+Play.current.configuration.getString("db.driver")
