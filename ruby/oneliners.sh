88e37f149045665b95cdc950004b53c75969d196
Fedor Sumkin <qosys.net@gmail.com>
2013-11-17 16:51:48 +0300

    Update oneliners.sh

--- ruby/oneliners.sh
+++ ruby/oneliners.sh
@@ -1,5 +1,6 @@
 # unique file (not a pipe version!!)
 ruby -e "puts IO.readlines(ARGV[0]).uniq" infile > outfile
 
-# Inplace replace of 'hello' to 'hello world' with creation of .bak file
+# (WIN) Inplace replace of 'hello' to 'hello world' with creation of .bak file
+# (LINUX) Remove .bak and no backup file will be created
 ruby -i.bak -pe "gsub('hello', 'hello world')" test.txt
