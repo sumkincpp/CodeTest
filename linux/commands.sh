cb7d6b5c9bef23e1b4886fefe431a2149b3bdb5b
Fedor Sumkin <qosys.net@gmail.com>
2014-01-19 19:45:05 +0300

    Update commands.sh

--- linux/commands.sh
+++ linux/commands.sh
@@ -1,3 +1,9 @@
 # Archiving
 
 zip -r backup.zip data
+
+# Code coloring with enscript
+
+enscript -2rG --line-numbers -p - --word-wrap --highlight=javascript --color=0 untitled.js | pstopdf -i -o ~/out.pdf && open ~/out.pdf
+
+enscript -1rG --line-numbers -p - --highlight=javascript --color=0 -c untitled.js | pstopdf -i -o ~/out.pdf && open ~/out.pdf
