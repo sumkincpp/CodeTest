c1f587ab730c0c97c869c22938ef17b95c92b1ec
Fedor Sumkin <qosys.net@gmail.com>
2014-01-22 09:50:34 +0300

    Update commands.sh

--- linux/commands.sh
+++ linux/commands.sh
@@ -6,6 +6,10 @@ tar -cf backup.tar data
 
 tar -czf backup.tar.gz data
 
+# unpacking to data folder
+mkdir data
+tar -cf backup.tar -C data
+
 # Code coloring with enscript
 
 enscript -2rG --line-numbers -p - --word-wrap --highlight=javascript --color=0 untitled.js | pstopdf -i -o ~/out.pdf && open ~/out.pdf
