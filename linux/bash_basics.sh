bee1712b31d2294f5c609b6aefe2373f8806eee2
Fedor Sumkin <qosys.net@gmail.com>
2014-01-28 17:10:42 +0300

    Update bash_basics.sh

--- linux/bash_basics.sh
+++ linux/bash_basics.sh
@@ -1,11 +1,34 @@
+####################################################
+#
+# Checking if bash variable is empty or not
+#
+####################################################
+
 VAR="hello"
 if [ -n "$VAR" ]; then
     echo "VAR is not empty"
 fi
 
-###############
+####################################################
 
 VAR=""
 if [ -z "$VAR" ]; then
     echo "VAR is empty"
 fi
+
+####################################################
+#
+# Listing files in folder
+#
+####################################################
+
+# Mistaken approach as every " " counts as separator :
+
+for i in `ls /opt/Docs`; do echo $i; done;
+
+# Proper approach, one file - one line :
+
+ls /opt/Docs | while read i; do echo $i; done;
+
+
+####################################################
