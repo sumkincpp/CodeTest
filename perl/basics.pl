165888d20219912ff5638bb6fe345b7917a1bbf0
Fedor Sumkin <qosys.net@gmail.com>
2014-04-07 00:36:28 +0400

    Update basics.pl

--- perl/basics.pl
+++ perl/basics.pl
@@ -20,3 +20,10 @@ __DATA__
 
 SOME data with #$%^&*( symbols, also tags
 and bla bla bla
+
+
+# ClearScreen -----------------------------------------
+
+print "\033[2J";   #clear the screen
+print "\033[0;0H"; #jump to 0,0
+
