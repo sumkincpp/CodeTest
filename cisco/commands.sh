956dc5d263d01dbc047f478fd7583a473bc9fe0b
Fedor Sumkin <qosys.net@gmail.com>
2014-04-24 10:18:19 +0400

    Create commands.sh

---
+++ cisco/commands.sh
@@ -0,0 +1,30 @@
+############################################################
+
+sh parser dump interface
+
+############################################################
+
+R1#sh parser dump interface | in dhcp
+0 no ip dhcp relay information trusted
+0 no ip dhcp relay information option subscriber-id
+0 no ip dhcp relay information option vpn-id
+0 no ip dhcp relay information option-insert
+0 no ip dhcp relay information check-reply
+0 no ip dhcp relay information policy-action
+0 no ip dhcp client default-router distance
+0 no ip dhcp client request router dns-nameserver
+0 no ip dhcp client request router domain-name
+0 no ip dhcp client request router static-route
+0 no ip dhcp client request router vendor-specific
+0 no ip dhcp client request router netbios-nameserver
+0 no ip dhcp client request router tftp-server-address
+0 no ip dhcp client request router
+0 no ip dhcp client request
+0 no ip dhcp client client-id
+0 no ip dhcp client class-id
+0 no ip dhcp client hostname
+0 no ip dhcp client route track
+0 no ip dhcp client lease
+0 no ip dhcp client mobile renew count <0-10> interval
+0 no ip dhcp client mobile renew count <0-10>
+0 no ip dhcp client mobile renew count
