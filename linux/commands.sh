a0d5baec31179f231dd0ef5dea5588eaec0abf7f
Fedor Sumkin <qosys.net@gmail.com>
2014-05-05 20:40:11 +0400

    Update commands.sh

--- linux/commands.sh
+++ linux/commands.sh
@@ -1,5 +1,4 @@
 # Archiving
-
 zip -r backup.zip data
 
 tar -cf backup.tar data
@@ -11,7 +10,9 @@ mkdir data
 tar -cf backup.tar -C data
 
 # Code coloring with enscript
-
 enscript -2rG --line-numbers -p - --word-wrap --highlight=javascript --color=0 untitled.js | pstopdf -i -o ~/out.pdf && open ~/out.pdf
 
 enscript -1rG --line-numbers -p - --highlight=javascript --color=0 -c untitled.js | pstopdf -i -o ~/out.pdf && open ~/out.pdf
+
+# Show PATH entries, one entry per line
+echo $PATH | tr : '\n'
