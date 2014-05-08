f04f8cf83d9c45d43de4ad5c2a57044c05cb33da
Fedor Sumkin <qosys.net@gmail.com>
2014-05-09 01:44:22 +0400

    Update commands.sh

--- macosx/commands.sh
+++ macosx/commands.sh
@@ -1,3 +1,8 @@
 # Mac OS X change shell
-
 chsh -s /bin/zsh
+
+# probably need to lish shell in /etc/shells
+$ echo '/usr/local/bin/fish' >> /etc/shells
+$ chsh -s /usr/local/bin/fish
+Changing shell for fedor.
+Password for fedor:
