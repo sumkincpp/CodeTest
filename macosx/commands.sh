18a3a7273d738c75c801179d80895fe9f7d33bf1
Fedor Sumkin <qosys.net@gmail.com>
2014-05-09 01:48:03 +0400

    Update commands.sh

--- macosx/commands.sh
+++ macosx/commands.sh
@@ -2,7 +2,7 @@
 chsh -s /bin/zsh
 
 # probably need to lish shell in /etc/shells
-$ echo '/usr/local/bin/fish' >> /etc/shells
+$ echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 $ chsh -s /usr/local/bin/fish
 Changing shell for fedor.
 Password for fedor:
