af36afa62f3b0150ffc7dbf4d84d84d07fa1e7ee
Fedor Sumkin <qosys.net@gmail.com>
2014-05-10 21:26:26 +0400

    Update macvlan.sh

--- linux/net/macvlan.sh
+++ linux/net/macvlan.sh
@@ -1,4 +1,4 @@
-sudo ip link add link eth0 mac0 type macvlan
-sudo ifconfig mac0 up
-sudo dhclient mac0
+sudo ip link add link eth0 macvlan0 type macvlan
+sudo ifconfig macvlan0 up
+sudo dhclient macvlan0
 sudo ip route add default via 192.168.1.1 
