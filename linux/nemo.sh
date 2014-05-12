70e01bd132d7c5907f5b636e1bcf9220d9f97fb2
Fedor Sumkin <qosys.net@gmail.com>
2014-05-12 16:59:55 +0400

    Update nemo.sh

--- linux/nemo.sh
+++ linux/nemo.sh
@@ -3,4 +3,5 @@
 # Hiding desktop icons for nemo! (Cinnamon)
 # https://wiki.archlinux.org/index.php/Nemo#Show_.2F_hide_desktop_icons
 #
+gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons false
