a87099fb913f95480807f7650094db760555c1a0
Fedor Sumkin <qosys.net@gmail.com>
2014-06-01 11:16:20 +0400

    Create commands.sh

---
+++ win/commands.sh
@@ -0,0 +1,11 @@
+# Provides list of started services
+net start
+
+# Get list of currently running tasks
+tasklist /svc
+
+# Disable/enable services
+msconfig
+
+# Get list of currently listening services
+netstat -ano
