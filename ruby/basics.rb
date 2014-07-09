9af9c71621b97ee1fa726b8bedb72681be646f75
Fedor Sumkin <qosys.net@gmail.com>
2014-07-09 20:10:33 +0400

    Update basics.rb

--- ruby/basics.rb
+++ ruby/basics.rb
@@ -17,3 +17,41 @@ if ary.grep('a'..'z').empty?
 else
   puts "Have permutation"
 end  
+
+"hello James!".downcase 
+"hello James!".upcase
+"hello James!".capitalize
+
+### Dates ###
+
+Date.today.month         # => current month(int)
+Date::MONTHNAMES[1]      # => January
+Date::ABBR_MONTHNAMES[1] # => JAN
+
+### Arrays / Enumerators ###
+
+(1..6).group_by { |i| i%3 } # => {0=>[3, 6], 1=>[1, 4], 2=>[2, 5]}
+(1..10).each_slice(3).to_a  # => [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
+
+
+### Files ###
+
+# File size in MBs
+compressed_file_size = '%.2f' % (File.size("file.txt").to_f / 2**20)
+# in bytes
+File.stat("testfile").size
+
+# Touch File
+require 'fileutils'
+FileUtils.touch('file.txt')  #1
+
+File.open("foo.txt", "w") {} #2
+File.write("foo.txt", "")    #3
+
+# Glob files
+
+Dir["config.?"]                     #=> ["config.h"]
+Dir.glob("config.?")                #=> ["config.h"]
+
+
+12.method("+").call(3) # => 15
