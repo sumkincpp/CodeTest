ccbd9e2ff2b15c7ed67730c14b11c4790243b5ff
Fedor Sumkin <qosys.net@gmail.com>
2014-07-09 15:30:07 +0400

    Create clean-all.cmake

---
+++ linux/programs/clean-all.cmake
@@ -0,0 +1,32 @@
+# Add a target that will ensure that the build directory is properly cleaned.
+add_custom_target(clean-all
+  COMMAND ${CMAKE_BUILD_TOOL} clean
+  COMMAND ${CMAKE_COMMAND} -P ${CMAKE_SCRIPT_PATH}/clean-all.cmake
+)
+
+# The helps us make sure that the build directory is *really* clean. 
+
+set(cmake_generated ${CMAKE_BINARY_DIR}/CMakeCache.txt
+                    ${CMAKE_BINARY_DIR}/cmake_install.cmake  
+                    ${CMAKE_BINARY_DIR}/Makefile
+                    ${CMAKE_BINARY_DIR}/CMakeFiles
+                    ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake
+                    ${CMAKE_BINARY_DIR}/CTestTestfile.cmake
+                    ${CMAKE_BINARY_DIR}/install_manifest.txt
+                    ${CMAKE_BINARY_DIR}/desktop/
+                    ${CMAKE_BINARY_DIR}/include/
+                    ${CMAKE_BINARY_DIR}/man/
+                    ${CMAKE_BINARY_DIR}/plug-ins/
+                    ${CMAKE_BINARY_DIR}/share/
+                    ${CMAKE_BINARY_DIR}/src/
+                    ${CMAKE_BINARY_DIR}/tests/
+                    ${CMAKE_BINARY_DIR}/utils/
+)
+
+foreach(file ${cmake_generated})
+
+  if (EXISTS ${file})
+     file(REMOVE_RECURSE ${file})
+  endif()
+
+endforeach(file)
