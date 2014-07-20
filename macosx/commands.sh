35b9f879a3d0d90bc7c574512537534c774a89cd
Fedor Sumkin <qosys.net@gmail.com>
2014-07-21 02:07:11 +0400

    Update commands.sh

--- macosx/commands.sh
+++ macosx/commands.sh
@@ -6,3 +6,8 @@ $ echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 $ chsh -s /usr/local/bin/fish
 Changing shell for fedor.
 Password for fedor:
+
+
+# Specifying Screenshots folder in Mac OS X
+fedor@Fedors-MacBook-Pro:~$ defaults write com.apple.screencapture location ~/Pictures/Screenshots/
+fedor@Fedors-MacBook-Pro:~$ killall SystemUIServer
