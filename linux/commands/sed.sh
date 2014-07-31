fb8678c609a0fc71e335b4a0d75d5e4d7b9f65c4
Fedor Sumkin <qosys.net@gmail.com>
2014-07-31 12:48:18 +0400

    Update sed.sh

--- linux/commands/sed.sh
+++ linux/commands/sed.sh
@@ -1,2 +1,8 @@
 $ echo " test test test  " | sed -re 's/(^ *| *$)//'
 test test test  
+
+# Sed lines from 10 to 20
+sed -n '10,20' 192.168.100.6_full.snmmpwalk
+
+# Sed to end of file
+sed -n '100,$p' 192.168.100.6_full.snmmpwalk
