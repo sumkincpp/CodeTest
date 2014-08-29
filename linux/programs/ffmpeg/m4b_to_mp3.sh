439991945bde1a9de3bf627a3e8a16cdb58b71fd
Fedor Sumkin <qosys.net@gmail.com>
2014-08-29 11:37:08 +0400

    Update m4b_to_mp3.sh

--- linux/programs/ffmpeg/m4b_to_mp3.sh
+++ linux/programs/ffmpeg/m4b_to_mp3.sh
@@ -1,1 +1,4 @@
 for m4b in *.m4b; do echo $m4b j; ffmpeg -i "$m4b" -acodec libmp3lame -ab 128k "${m4b}.mp3"; done
+
+# another sample
+for m4b in $(ls -1 *.m4b); do ffmpeg -i $m4b -acodec libmp3lame -ar 22050 ${m4b}.mp3; done
