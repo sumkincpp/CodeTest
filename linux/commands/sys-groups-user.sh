19612826cc35725347a9e1e31cd9db69ae14fb34
Fedor Sumkin <qosys.net@gmail.com>
2014-09-03 21:32:38 +0400

    Update sys-groups-user.sh

--- linux/commands/sys-groups-user.sh
+++ linux/commands/sys-groups-user.sh
@@ -10,3 +10,19 @@ vagrant wheel
 # Getting primary group
 [root@vagrant-centos65 ~]# getent group vagrant
 vagrant:x:500:
+
+[root@vagrant-centos65 ~]# groupadd developers
+[root@vagrant-centos65 ~]# usermod -a -G developers vagrant
+[root@vagrant-centos65 ~]# groups vagrant
+vagrant : vagrant wheel developers
+
+# Changing primary group
+[root@vagrant-centos65 ~]# usermod -g www tony
+
+# Adding user
+useradd -G admins,developers jerry
+
+# USing id
+[root@vagrant-centos65 ~]# id vagrant
+uid=500(vagrant) gid=500(vagrant) groups=500(vagrant),10(wheel),502(developers)
+
