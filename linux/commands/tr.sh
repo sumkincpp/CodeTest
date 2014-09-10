99711d05294252a5d765fc432788414de8102349
Fedor Sumkin <qosys.net@gmail.com>
2014-09-10 10:54:18 +0400

    Update tr.sh

--- linux/commands/tr.sh
+++ linux/commands/tr.sh
@@ -4,3 +4,9 @@ tr '[A-Z]' '[a-z]'
 
 # deletes charachters
 tr -d piu
+
+# Squeze repeated chars
+smt | tr -s ' ' | cut -d ' ' -f 4
+# -s, --squeeze-repeats   replace each input sequence of a repeated character
+#                         that is listed in SET1 with a single occurrence
+#                         of that character
