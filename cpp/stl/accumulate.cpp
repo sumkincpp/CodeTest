87bd4e2cfb2f0724756f93df69c2ae02cb3b02e5
Fedor Sumkin <qosys.net@gmail.com>
2014-12-07 16:00:14 +0300

    Update accumulate.cpp

--- cpp/stl/accumulate.cpp
+++ cpp/stl/accumulate.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main() {
+int main()
+{
+  int arr[] = {0, 1, 2, 3, 4, 5};
+  
+  
+  int total = std::accumulate(arr, arr+6, 0);
+  std::cout << total << endl; 
+  
+  int sum  = 0;
+  std::for_each(arr, arr+6, [&sum](int n){ sum += n; });
+  std::cout << sum;
+  
+  sum  = 0;
+  std::cout << std::for_each(arr,arr+6,[&](int n)->int{sum += n;return sum;})(0);
+  
   return 0;
 }
