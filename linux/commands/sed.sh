2d15612dafc09a93604dccb16846e4c2f68dd529
Fedor Sumkin <qosys.net@gmail.com>
2015-01-21 14:42:22 +0300

    Update sed.sh

--- linux/commands/sed.sh
+++ linux/commands/sed.sh
@@ -9,3 +9,6 @@ sed -n '100,$p' 192.168.100.6_full.snmmpwalk
 
 # replace foo to bar in file, not single quotes - dat's for MAC OS(bsd?)
 sed -i '' -e 's/foo/bar/' target.file
+
+#sed and print next 10-th line
+sed -n '/hehee/{10n;p;}' target.file
