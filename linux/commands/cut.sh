c4e612ca85cf045bd758e9729bd04859510f82e1
Fedor Sumkin <qosys.net@gmail.com>
2015-02-02 11:57:46 +0300

    Update cut.sh

--- linux/commands/cut.sh
+++ linux/commands/cut.sh
@@ -1,3 +1,7 @@
 
 # Splitting by "." and displaying all fields in range from 1 to 9
 cat p1.cpk.mngmt.snap | cut -d'.' -f1-9 | uniq
+
+# yet another cut with delimeter, yau 
+echo "fsdfsd: fsdfs: fsdfds" | cut -d: -f2-
+ fsdfs: fsdfds
