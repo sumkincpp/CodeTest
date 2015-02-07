64f684652c847cf6cf173686914262338a0c1938
Fedor Sumkin <qosys.net@gmail.com>
2015-02-07 14:12:33 +0300

    Update git.sh

--- linux/programs/git.sh
+++ linux/programs/git.sh
@@ -1,3 +1,9 @@
 
 # clone with one commit depth
 git clone --depth=1 <blablabla>
+
+# diff file on -2 revision
+git diff HEAD~2 spin_glass_counter.hpp
+
+# show file on -2 revision
+git show HEAD~2:./spin_glass_counter.hpp
