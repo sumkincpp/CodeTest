aebf4f839aed07772521b2ff5136edad16e76b03
Fedor Sumkin <qosys.net@gmail.com>
2015-02-25 17:09:15 +0300

    Update and rename scala to scala/sbt.sh

---
+++ scala/sbt.sh
@@ -0,0 +1,7 @@
+C:\Code\TheCode>sbt about
+[info] Set current project to thecode (in build file:/C:/Code/TheCode/)
+[info] This is sbt 0.13.7
+[info] The current project is {file:/C:/Code/TheCode/}thecode 0.1-SNAPSHOT
+[info] The current project is built against Scala 2.10.4
+[info] Available Plugins: sbt.plugins.IvyPlugin, sbt.plugins.JvmPlugin, sbt.plugins.CorePlugin, sbt.plugins.JUnitXmlReportPlugin
+[info] sbt, sbt plugins, and build definitions are using Scala 2.10.4
