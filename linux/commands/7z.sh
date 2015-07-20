ccf473f28aa7b0403db8abae89cb439e3bbbdf6e
Fedor Sumkin <qosys.net@gmail.com>
2015-07-20 09:31:16 +0300

    Update 7z.sh

--- linux/commands/7z.sh
+++ linux/commands/7z.sh
@@ -1,2 +1,8 @@
 #maximum compression 
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on archive.7z piupiupiu/
+
+#exclude by file list
+'-xr@\path\to\backup_daily_exclude.lst'
+
+#exclude by directory pattern
+-x?!ddd*
