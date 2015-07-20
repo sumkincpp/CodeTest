58a4bddc7105f09d8fc66b209ee3a548addda778
Fedor Sumkin <qosys.net@gmail.com>
2015-07-20 09:35:31 +0300

    Update 7z.sh

--- linux/commands/7z.sh
+++ linux/commands/7z.sh
@@ -2,7 +2,10 @@
 7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on archive.7z piupiupiu/
 
 #exclude by file list
+# -r -- recursive
 '-xr@\path\to\backup_daily_exclude.lst'
 
 #exclude by directory pattern
 -x?!ddd*
+
+7za a test.7z /tmp/test -x\!test/1/*
