4499a2c8474182d16e7079b647278e9cfe772f72
Fedor Sumkin <qosys.net@gmail.com>
2015-08-31 10:59:32 +0300

    Update ethtool.sh

--- linux/commands/ethtool.sh
+++ linux/commands/ethtool.sh
@@ -1,2 +1,23 @@
+# ethtool eth0
+Settings for eth0:
+        Supported ports: [ TP ]
+        Supported link modes:   1000baseT/Full
+                                10000baseT/Full
+        Supported pause frame use: No
+        Supports auto-negotiation: No
+        Advertised link modes:  Not reported
+        Advertised pause frame use: No
+        Advertised auto-negotiation: No
+        Speed: 10000Mb/s
+        Duplex: Full
+        Port: Twisted Pair
+        PHYAD: 0
+        Transceiver: internal
+        Auto-negotiation: off
+        MDI-X: Unknown
+        Supports Wake-on: uag
+        Wake-on: d
+        Link detected: yes
+
 # ethtool -P eth0
 Permanent address: 00:00:00:00:00:00
