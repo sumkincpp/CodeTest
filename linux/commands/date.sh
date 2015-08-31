3dae3e09092612cf2edba2ad1f8b4efc9b30904f
Fedor Sumkin <qosys.net@gmail.com>
2015-08-31 10:31:27 +0300

    Update date.sh

--- linux/commands/date.sh
+++ linux/commands/date.sh
@@ -11,3 +11,9 @@ perl -MTime::HiRes -e 'printf(\"%.0f\n\",Time::HiRes::time()*1000)'
 date -d @1223333333
 
 date -d @1278999698 +'%Y-%m-%d %H:%M:%S'
+
+TZ=America/New_York date
+Mon Aug 31 03:30:54 EDT 2015
+
+TZ=Europe/Moscow date
+Mon Aug 31 10:31:03 MSK 2015
