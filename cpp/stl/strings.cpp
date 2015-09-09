bcce1a1274d86a186080af0e311581cc695f7ef9
Fedor Sumkin <qosys.net@gmail.com>
2015-09-09 11:01:19 +0300

    Update strings.cpp

--- cpp/stl/strings.cpp
+++ cpp/stl/strings.cpp
@@ -11,3 +11,24 @@ str.erase(end_pos, str.end());
 string.erase(std::remove_if(string.begin(), string.end(), std::isspace), string.end()); // ? is _if legal
 
 //-----------------------------------------------------
+// Splitting string to vector with char delimiter
+#include <string>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
