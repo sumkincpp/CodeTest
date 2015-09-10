f9c599c250b189d225bab759b004f93c432b2713
Fedor Sumkin <qosys.net@gmail.com>
2015-09-10 12:17:10 +0300

    Update vector-partials.cpp

--- cpp/stl/vector-partials.cpp
+++ cpp/stl/vector-partials.cpp
@@ -3,3 +3,6 @@
 a.insert(a.end(), b.begin(), b.end());
 
 std::copy (b.begin(), b.end(), std::back_inserter(a));
+
+// printing vec
+std::copy(folders_vec.begin(), folders_vec.end(), std::ostream_iterator<std::string>(std::cout,", "));
