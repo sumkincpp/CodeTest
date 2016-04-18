2b96c1b2d4de5dbfaed8022b3f97bea484cab1ee
Fedor Sumkin <qosys.net@gmail.com>
2016-04-18 18:00:37 +0300

    Create netbps.pl
    tcpdump -> bytes per/sec

---
+++ linux/programs/tcpdump/netbps.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+#
+# usage: sudo tcpdump -i any -l -e -n host XXX and port YYY | ./netbps.pl
+#
+use strict;
+use warnings;
+use Time::HiRes;
+use IO::Handle;
+
+my $reporting_interval = 10.0; # seconds
+my $bytes_this_interval = 0;
+my $start_time = [Time::HiRes::gettimeofday()];
+
+STDOUT->autoflush(1);
+
+while (<>) {
+  if (/ length (\d+):/) {
+    $bytes_this_interval += $1;
+    my $elapsed_seconds = Time::HiRes::tv_interval($start_time);
+    if ($elapsed_seconds > $reporting_interval) {
+       my $bps = $bytes_this_interval / $elapsed_seconds;
+       printf "%02d:%02d:%02d %10.2f Bps\n", (localtime())[2,1,0],$bps;
+       $start_time = [Time::HiRes::gettimeofday()];
+       $bytes_this_interval = 0;
+    }
+  }
+}
