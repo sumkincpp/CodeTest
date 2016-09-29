fbdd4dceecb9f4f1fba0d22aaad1ad2105ccfc36
Fedor Sumkin <qosys.net@gmail.com>
2016-09-29 22:24:55 +0300

    Update git.sh

--- linux/programs/git.sh
+++ linux/programs/git.sh
@@ -3,6 +3,15 @@ system-wide/user-wide/project-wide
 
 git config --list
 
+git config --global user.name "<your_full_name>"
+git config --global user.email "<your_email>"
+git config --global core.editor "atom --wait"
+
+# "auto carriage return line feed".
+git config --global core.autocrlf
+# Windows - true
+# Mac/Linux - input
+
 # clone with one commit depth
 git clone --depth=1 <blablabla>
 
