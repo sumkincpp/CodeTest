887f708b7faeed0b94933cbf57c003bc562cd236
Fedor Sumkin <qosys.net@gmail.com>
2016-10-16 21:24:07 +0300

    better version for retrieveing connections table

--- powershell/netstat-abno.ps1
+++ powershell/netstat-abno.ps1
@@ -1,11 +1,21 @@
 # fully `netstat -abno` on powershell
-
+#
+# Get-NetTCPConnection
+# https://technet.microsoft.com/en-us/library/hh826153.aspx
+#
+# MSFT_NetTCPConnection, ROOT\StandardCimv2
+# http://wutils.com/wmi/root/standardcimv2/msft_nettcpconnection/
+#
+# Where-Object -Property State -EQ -Value 'Established'
 Get-NetTCPConnection | ForEach-Object -Process {
+    $process = (Get-Process -Id $_.OwningProcess)
     [PSCustomObject] @{
-        'ProcessName' = (Get-Process -Id $_.OwningProcess).Name
-        'ProcessId' = (Get-Process -Id $_.OwningProcess).Id
-        'RemoteIP'  = $_.RemoteAddress
-        'LocalPort' = $_.LocalPort
-        'State'     = $_.State
+        'Local Address' = $_.LocalAddress
+        'LocalPort'     = $_.LocalPort
+        'RemoteIP'      = $_.RemoteAddress
+        'RemotePort'    = $_.RemotePort
+        'ProcessId'     = $process.Id
+        'ProcessName'   = $process.Name
+        'State'         = $_.State
     }
-}
+} | Sort-Object State -descending
