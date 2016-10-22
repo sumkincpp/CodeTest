f7af1c8971574c64bed074686d42b8b7e1532a6b
Fedor Sumkin <qosys.net@gmail.com>
2016-10-22 20:28:16 +0400

    Update example.sh

--- macosx/osascript/example.sh
+++ macosx/osascript/example.sh
@@ -1,12 +1,14 @@
 
-osascript -e 'tell app "Finder" to get disks'
 
 osascript -e "the clipboard"
 
+osascript -e "set volume output volume 15"
 osascript -e 'set volume output muted true'
 osascript -e 'set volume output muted false'
 
+osascript -e 'tell app "Finder" to get disks'
 osascript -e 'tell app "Finder" to empty trash'
+osascript -e 'tell app "Finder" to restart'
 
 osascript -e 'quit application "GrowlSafari"'
 osascript -e 'tell application "XXX" to hide'
