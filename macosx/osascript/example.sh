7449bb8498f80b9f593cb3df7338b68adf325180
Fedor Sumkin <qosys.net@gmail.com>
2016-10-22 20:27:12 +0400

    Update example.sh

--- macosx/osascript/example.sh
+++ macosx/osascript/example.sh
@@ -9,6 +9,7 @@ osascript -e 'set volume output muted false'
 osascript -e 'tell app "Finder" to empty trash'
 
 osascript -e 'quit application "GrowlSafari"'
+osascript -e 'tell application "XXX" to hide'
 
 # Speeking what are you~
 osascript -e 'say "what are you doing" using "alex"'
