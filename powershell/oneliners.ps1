fa446ca5aa839df1e70c7ef4bb88ec867ddf48ef
Fedor Sumkin <qosys.net@gmail.com>
2016-10-23 21:08:45 +0300

    Update oneliners.ps1

--- powershell/oneliners.ps1
+++ powershell/oneliners.ps1
@@ -2,3 +2,9 @@
 
 # similar to netstat command, but it returns an oject instead of a string.
 Get-NetTCPConnection -State Listen | ft state,l*port, l*address, r*port, r*address –Auto
+
+# Current Hostname
+$env:computername
+Hostname
+(Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
+get-childitem -path env:computername
