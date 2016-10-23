b9afe5afaabfb499a246ea11a968a624882e65e2
Fedor Sumkin <qosys.net@gmail.com>
2016-10-23 19:40:52 +0400

    Update wmic.sh

--- win/wmic.sh
+++ win/wmic.sh
@@ -4,9 +4,6 @@ wmic startup
 wmic useraccount list
 wmic group list full
 
-wmic csproduct get uuid
-wmic csproduct get name
-wmic csproduct get vendor,name,identifyingnumber
 
 wmic diskdrive get status
 wmic bios get serialnumber
@@ -14,10 +11,24 @@ wmic logicaldisk get name, volumename
 wmic Memcache list brief
 
 wmic ComputerSystem get TotalPhysicalMemory
+wmic computersystem get TotalPhysicalMemory /VALUE /FORMAT:VALUE
+
+# csproduct
+
+wmic csproduct get uuid
+wmic csproduct get name
+wmic csproduct get vendor,name,identifyingnumber
+
+# OS
 
 wmic OS get TotalVirtualMemorySize
+wmic os get freephysicalmemory /VALUE /FORMAT:VALUE
+wmic OS Get LastBootUpTime | findstr +
+
+# product
 
 wmic product get name
+wmic product where "name like '%%ProgramName%%'" call uninstall /nointeractive
 
 wmic useraccount where name='Administrator' call Rename admin
 
@@ -47,6 +58,7 @@ wmic cpu get name
 wmic cpu get name /Value
 wmic CPU get NumberOfLogicalProcessors
 wmic CPU get NumberOfCores
+wmic cpu get loadpercentage /VALUE /FORMAT:VALUE
 
 # motherboard
 
