d1837e82f57c145df4d199fcababb3884c08dfc6
Fedor Sumkin <qosys.net@gmail.com>
2016-11-01 12:50:18 +0400

    Create firefox-search.sh

---
+++ macosx/osascript/firefox-search.sh
@@ -0,0 +1,4 @@
+osascript -e 'tell app "Firefox" to activate'
+osascript -e 'tell application "System Events" to keystroke "t" using command down'
+osascript -e 'tell application "System Events" to keystroke "hellow"'
+osascript -e 'tell application "System Events" to key code 76'
