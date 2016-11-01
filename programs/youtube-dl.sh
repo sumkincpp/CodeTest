173edc53aef35c1ad318f00d530a1203749377b2
Fedor Sumkin <qosys.net@gmail.com>
2016-11-01 16:25:47 +0300

    Update youtube-dl.sh

--- programs/youtube-dl.sh
+++ programs/youtube-dl.sh
@@ -1,2 +1,3 @@
-youtube-dl --list-subs <link>
-youtube-dl --all-subs --skip-download <link>
+youtube-dl --list-subs URL
+youtube-dl --all-subs --skip-download URL
+youtube-dl --skip-download --write-srt --sub-lang en URL
