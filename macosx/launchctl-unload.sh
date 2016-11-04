a6dd3df000e0d346337434d8635bf81b536242fa
Fedor Sumkin <qosys.net@gmail.com>
2016-11-04 18:11:59 +0400

    Update launchctl-unload.sh

--- macosx/launchctl-unload.sh
+++ macosx/launchctl-unload.sh
@@ -1,3 +1,4 @@
 launchctl unload -w /System/Library/LaunchAgents/com.apple.CalendarAgent.plist
 launchctl unload -w /System/Library/LaunchAgents/com.apple.gamed.plist
 launchctl unload -w /System/Library/LaunchAgents/com.apple.AddressBook.SourceSync.plist
+launchctl unload -w /System/Library/LaunchDaemons/com.apple.apsd.plist
