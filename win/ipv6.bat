c032f7474d72e6058bb26036efe2e835a53a1781
Fedor Sumkin <qosys.net@gmail.com>
2017-08-28 13:10:41 +0300

    Update ipv6.bat

--- win/ipv6.bat
+++ win/ipv6.bat
@@ -7,3 +7,21 @@ netsh int ipv6 show neigh
 REM Possible fix for ipv6 tcp/ip stack issues
 netsh int ip reset
 netsh winsock reset
+
+
+REM Additional flush/reset options
+netsh interface ipv6 reset
+netsh advfirewall reset
+
+REM disabling Teredo tunnel 
+netsh int ipv6 isatap set state disabled
+netsh int ipv6 6to4 set state disabled
+netsh int teredo set state disabled
+
+REM resetting full stack
+ipconfig /flushdns
+nbtstat -R
+nbtstat -RR
+netsh int reset all
+netsh int ip reset
+netsh winsock reset
