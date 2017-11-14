e4ff3d6d5d5ad1676c0c2e0ccf0f7a7dfac88ce1
Fedor Sumkin <qosys.net@gmail.com>
2017-11-14 20:39:49 +0300

    Update normalize-filenames.sh

--- linux/oneliners/normalize-filenames.sh
+++ linux/oneliners/normalize-filenames.sh
@@ -1,2 +1,2 @@
 # find all files with ':' in naming and remove that damn sign
-find . | grep -a : | tr '\n' '\0' | xargs -0 -n1 -I%% sh -c "mv '%%' \$(echo '%%' | tr -d :)"
+find . | grep -a '[:?]' | tr '\n' '\0' | xargs -0 -n1 -I%% sh -c "mv '%%' \$(echo '%%' | tr -d ':?')"
