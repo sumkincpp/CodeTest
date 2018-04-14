088087260902d7b11d2dc328490b32f8d0113440
Fedor Sumkin <qosys.net@gmail.com>
2018-04-14 12:19:24 +0300

    Update firewall.sh

--- linux/centos7/firewall.sh
+++ linux/centos7/firewall.sh
@@ -1,3 +1,20 @@
+
+sudo firewall-cmd --get-zones
+sudo firewall-cmd --list-all-zones
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --get-services
+
+sudo firewall-cmd --zone=public --list-services
+
+# Logging denieds packets - /var/log/messages
+sudo firewall-cmd --set-log-denied=all
+
+# Adding service
+sudo firewall-cmd --add-service=dhcp --permanent
+
 # Opening port :
 sudo firewall-cmd --zone=dmz --add-port=2888/tcp --permanent
+
 sudo firewall-cmd --reload
+
+
