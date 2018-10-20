2a2b91e1d7aa5fce4831800038e61a5f8cb65dfc
Fedor V <qosys.net@gmail.com>
2018-10-20 10:51:59 +0300

    Update bash_basics.sh

--- linux/bash_basics.sh
+++ linux/bash_basics.sh
@@ -79,3 +79,29 @@ $ date "+%T %d %b %Y"
 for f in `ls`; do
   echo "File -> $f"
 done
+
+####################################################
+# Array 1
+####################################################
+farm_hosts=(web03 web04 web05 web06 web07)
+
+for i in ${farm_hosts[@]}; do
+  echo $i
+done
+
+####################################################
+# Array 2
+####################################################
+
+## declare an array variable
+declare -a arr=("element1" "element2" "element3")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+done
+
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
+####################################################
