19e4fdb7db26f8c4c3cd6b8cb31612ea5761b7cb
Fedor <qosys.net@gmail.com>
2020-12-06 14:18:41 +0300

    find examples + .gitignore

--- linux/commands/find.sh
+++ linux/commands/find.sh
@@ -6,3 +6,7 @@ fedor@as5930 ~/tmp $ find . -mtime +1
 
 #less than 1 day
 fedor@as5930 ~/tmp $ find . -mtime -1
+
+# chmod all folders
+find . -type d -exec chmod 755 {} \;
+find /opt/lampp/htdocs -type f -exec chmod 644 {} \;
