c81a474c571daab0b42404379dbe59e91f8fb360
Fedor V <qosys.net@gmail.com>
2020-12-06 15:02:20 +0300

    find command fix

--- linux/commands/find.sh
+++ linux/commands/find.sh
@@ -9,4 +9,4 @@ fedor@as5930 ~/tmp $ find . -mtime -1
 
 # chmod all folders
 find . -type d -exec chmod 755 {} \;
-find /opt/lampp/htdocs -type f -exec chmod 644 {} \;
+find . -type f -exec chmod 644 {} \;
