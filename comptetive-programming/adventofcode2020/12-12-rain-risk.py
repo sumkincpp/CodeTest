54dbd076715b7a55e3c073d17b59728c5972cb27
Fedor V <qosys.net@gmail.com>
2020-12-12 19:43:44 +0300

    Advent Of Code 2020 Day 12 ~ part2 ~ quick & easy
    Q:      How many WASPs does it take to change a light bulb?
    A:      One.

--- comptetive-programming/adventofcode2020/12-12-rain-risk.py
+++ comptetive-programming/adventofcode2020/12-12-rain-risk.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-# You are the only person to ever get this message.
+#
+# Q:      How many WASPs does it take to change a light bulb?
+# A:      One.
+#
 
 import re
 import sys
@@ -33,22 +36,26 @@ class MoveDirection(str, Enum):
     LEFT = "L"
 
 
+DIRECTIONS = [
+    MoveDirection.NORTH,
+    MoveDirection.WEST,
+    MoveDirection.SOUTH,
+    MoveDirection.EAST,
+]
+NUM_DIRECTIONS = len(DIRECTIONS)
+
+# 0 -> no rotate
+# 1 -> right
+# -1 -> left
+ROTATION = [
+    [(1, 0), (0, 1)],
+    [(0, -1), (1, 0)],  # 90
+    [(-1, 0), (0, -1)],
+    [(0, 1), (-1, 0)],  # 270
+]
+
+
 def ship_moves_manhattan_distance(moves):
-    dirs = [
-        MoveDirection.NORTH,
-        MoveDirection.WEST,
-        MoveDirection.SOUTH,
-        MoveDirection.EAST,
-    ]
-    num_dir = len(dirs)
-    # left_dirs = {d: dirs[(i + 1) % len(dirs)] for i, d in enumerate(dirs)}
-    # right_dirs = {d: dirs[i - 1] for i, d in enumerate(dirs)}
-
-    # print(left_dirs)
-    # print(right_dirs)
-
-    # sys.exit(0)
-
     x = 0
     y = 0
 
@@ -61,11 +68,11 @@ def ship_moves_manhattan_distance(moves):
         step = int(matches.group(2))
 
         if new_dir == MoveDirection.LEFT:
-            curr_idx = dirs.index(curr_dir)
-            curr_dir = dirs[(curr_idx + step // 90) % num_dir]
+            curr_idx = DIRECTIONS.index(curr_dir)
+            curr_dir = DIRECTIONS[(curr_idx + step // 90) % NUM_DIRECTIONS]
         elif new_dir == MoveDirection.RIGHT:
-            curr_idx = dirs.index(curr_dir)
-            curr_dir = dirs[(curr_idx - step // 90) % num_dir]
+            curr_idx = DIRECTIONS.index(curr_dir)
+            curr_dir = DIRECTIONS[(curr_idx - step // 90) % NUM_DIRECTIONS]
         else:
             # When moving forward,
             # - translate new_dir to use curr_dir
@@ -90,6 +97,69 @@ def ship_moves_manhattan_distance(moves):
     return abs(x) + abs(y)
 
 
+def waypoint_moves_manhattan_distance(moves):
+    """
+    Action N means to move the waypoint north by the given value.
+    Action S means to move the waypoint south by the given value.
+    Action E means to move the waypoint east by the given value.
+    Action W means to move the waypoint west by the given value.
+    Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+    Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+    Action F means to move forward to the waypoint a number of times equal to the given value.
+    """
+
+    # waypoint
+    wx = 10
+    wy = 1
+
+    # our ship
+    x = 0
+    y = 0
+
+    for line in moves:
+        print(line)
+        matches = re.match(r"^([A-Z]+)([0-9]+)$", line)
+
+        new_dir = matches.group(1)
+        step = int(matches.group(2))
+
+        if new_dir == MoveDirection.LEFT:
+            matrix = ROTATION[(+step // 90) % NUM_DIRECTIONS]
+
+            wx2 = wx * matrix[0][0] + wy * matrix[0][1]
+            wy2 = wx * matrix[1][0] + wy * matrix[1][1]
+
+            wx, wy = wx2, wy2
+        elif new_dir == MoveDirection.RIGHT:
+            matrix = ROTATION[(-step // 90) % NUM_DIRECTIONS]
+
+            wx2 = wx * matrix[0][0] + wy * matrix[0][1]
+            wy2 = wx * matrix[1][0] + wy * matrix[1][1]
+
+            wx, wy = wx2, wy2
+        else:
+            # moving forward
+            if new_dir == MoveDirection.FORWARD:
+                x += wx * step
+                y += wy * step
+            elif new_dir == MoveDirection.NORTH:
+                wy += step
+            elif new_dir == MoveDirection.SOUTH:
+                wy -= step
+            elif new_dir == MoveDirection.WEST:
+                wx -= step
+            elif new_dir == MoveDirection.EAST:
+                wx += step
+            else:
+                raise ValueError("What' up?")
+
+        print("x={} y={} wx={} wy={}".format(x, y, wx, wy))
+
+    print("x={} y={}".format(x, y))
+
+    return abs(x) + abs(y)
+
+
 @pytest.mark.parametrize(
     'lines,expected',
     [
@@ -100,6 +170,16 @@ def test_ship_moves_manhattan_distance(lines, expected):
     assert ship_moves_manhattan_distance(lines) == expected
 
 
+@pytest.mark.parametrize(
+    'lines,expected',
+    [
+        (BASE_TEST, 286),
+    ],
+)
+def test_waypoint_moves_manhattan_distance(lines, expected):
+    assert waypoint_moves_manhattan_distance(lines) == expected
+
+
 if __name__ == "__main__":
-    num = ship_moves_manhattan_distance(sys.stdin)
+    num = waypoint_moves_manhattan_distance(sys.stdin)
     print("{0}".format(num))
