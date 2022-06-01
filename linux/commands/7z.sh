871771c5e9e6169869475275702a83aa4c592ba0
Fedor V <qosys.net@gmail.com>
2022-06-01 08:37:01 +0200

    Update 7z.sh

--- linux/commands/7z.sh
+++ linux/commands/7z.sh
@@ -1,7 +1,25 @@
-#maximum compression 
-7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on archive.7z piupiupiu/
+# 7-Zip commands
 
-#exclude by file list
+```
+sudo apt install p7zip-full
+```
+
+## .7z
+
+```
+7za a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on archive.7z piupiupiu/
+```
+
+## .zip
+
+```
+7z a -mm=Deflate -mfb=258 -mpass=15 -r foo.zip
+```
+
+## Exclude 
+
+```
+# exclude by file list
 # -r -- recursive
 '-xr@\path\to\backup_daily_exclude.lst'
 
@@ -9,3 +27,4 @@
 -x?!ddd*
 
 7za a test.7z /tmp/test -x\!test/1/*
+```
